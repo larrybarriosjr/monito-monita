528a2a678f4c8dae1b866a13f07987259842bd7f
Larry Barrios Jr <github@larrybarriosjr.com>
2021-12-03 22:22:17 +0800

    Setup Netlify Functions

---
+++ src/setupProxy.js
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { createProxyMiddleware } = require("http-proxy-middleware")
+
+module.exports = app => {
+  app.use(
+    createProxyMiddleware("/.netlify/functions", {
+      target: "http://localhost:9000",
+      pathRewrite: {
+        "^/\\.netlify/functions": "",
+      },
+    }),
+  )
+}
