65395a93f5ed93e7691b35343e61eaf987113111
Larry Barrios Jr <github@larrybarriosjr.com>
2021-12-05 17:27:49 +0800

    Verify whose password is sent

--- functions/login.js
+++ functions/login.js
@@ -7,12 +7,21 @@ const client = new faunadb.Client({
 
 exports.handler = (event, _context, callback) => {
   const data = JSON.parse(event.body)
+  const currentYear = new Date().getFullYear().toString()
   return client
-    .query(q.Get(q.Match(q.Index("password_by_name"), data)))
+    .query(
+      q.Map(
+        q.Filter(
+          q.Paginate(q.Match(q.Index("passwords_by_year"), currentYear)),
+          q.Lambda(x => q.Equals(data, q.Select(["data", "code"], q.Get(x)))),
+        ),
+        q.Lambda(x => q.Select(["data", "member"], q.Get(x))),
+      ),
+    )
     .then(response => {
       return callback(null, {
         statusCode: 200,
-        body: JSON.stringify(response),
+        body: JSON.stringify(response.data[0]),
       })
     })
     .catch(error => {
